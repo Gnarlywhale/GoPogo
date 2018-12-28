09aeac294b8aebbe2564dac2e269b9dc1120bba8
Gnarlywhale <trd@ualberta.ca>
2018-12-28 02:27:53 -0700

    Able to launch on droid now

---
+++ FetchTest/app.js
@@ -0,0 +1,17 @@
+const https = require('https');
+https.get('https://fleet.invers.com/pogo/App/ItemsOnMap/ItemsOnMap.aspx', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
