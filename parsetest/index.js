e1d7872122767decab06c0c479111332d9f14cfb
Riley <gnarlywhale@live.co.uk>
2019-01-04 18:24:48 -0700

    Seems to work in parse tester code :/

--- parsetest/index.js
+++ parsetest/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var str = fs.readFileSync("source.html", "utf8");
 p5 = require('parse5');
+parsed = p5.parse(str);
 console.log(parsed.childNodes[1].childNodes[2].childNodes[1].childNodes[1].childNodes[44].childNodes[0].value);
 
 // var parser = new parse5.Parser();
